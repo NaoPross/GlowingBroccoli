78d5f5cda21bd64112730926d3cf5192a43b488d
Steven Wegmann <steven.wegmann@gmail.com>
2020-10-05 14:54:28 +0200

    Implement move, grid and tail

--- src/snake.h
+++ src/snake.h
@@ -3,27 +3,31 @@
 
 #include <QGraphicsItem>
 #include <QPainter>
+#include <QKeyEvent>
 
-class Snake : public QGraphicsItem
+class Snake : public QGraphicsObject
 {
 public:
     Snake();
     virtual ~Snake();
 
-    QRectF boundingRect () const {
-        return QRectF(-300,-300,600,600);
-    }
-
-    void paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget);
+    QRectF boundingRect () const override;
 
 protected:
     void timerEvent(QTimerEvent *event);
+    void paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget);
 
 private:
+    static const int RECT_SIZE = 20;
     static const int MAX_SIZE = 100;
+    static const int MIN_SIZE = 3;
+    static const int DELAY = 120;
 
     int x[MAX_SIZE];
     int y[MAX_SIZE];
+    int direction;
+    int tail;
+    int timerId;
 
     void moveSnake();
 };
