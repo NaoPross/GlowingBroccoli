e51047e54730ddb1a9d83b3b07ed694614601b4a
Nao Pross <naopross@thearcway.org>
2020-10-05 22:15:06 +0200

    Refractor Snake header

--- src/snake.h
+++ src/snake.h
@@ -5,31 +5,111 @@
 #include <QPainter>
 #include <QKeyEvent>
 
+/// \brief Helper class to manage a dynamically allocated grid
+///
+/// Grid is a non-copyable non-movable template class to
+/// generate a dynamically allocated grid or matrix of type T
+template<typename T>
+class Grid
+{
+public:
+    const unsigned width;
+    const unsigned height;
+
+    Grid() = delete;
+    Grid(const Grid& other) = delete;
+    Grid(Grid&& other) = delete;
+
+    /// \brief Create a grid of width w and height h
+    /// \param w the width / number of columns of the grid
+    /// \param h the height / number of rows of the grid
+    /// \param default_value the default value for the elements of the grid
+    Grid(unsigned w, unsigned h, T default_value)
+        : width(w), height(h)
+    {
+        grid = new T*[height];
+        for (unsigned row = 0; row < height; row++) {
+            grid[row] = new T[height];
+            for (int col = 0; col < width; col++) {
+                grid[row][col] = default_value;
+            }
+        }
+    }
+
+    ~Grid() {
+        for (unsigned row = 0; row < height; row++) {
+            delete[] grid[row];
+        }
+
+        delete[] grid;
+    }
+
+    /// \brief Get an element from the grid.
+    /// \param row the row / y coordinate in the grid
+    /// \param col the column / x coordinate in the grid
+    /// \return Mutable reference to the cell
+    T& get(unsigned row, unsigned col) {
+        if (row < height && col < width) {
+            return grid[row][col];
+        }
+        // TODO: throw an exception or change the rv to optional<T>
+        return static_cast<T>(0);
+    }
+
+    /// \sa Grid<T>::get
+    const T& get(unsigned row, unsigned col) const {
+        return get(row, col);
+    }
+
+private:
+    T **grid;
+};
+
+/// \brief Graphic object that draws the game of Snake
 class Snake : public QGraphicsObject
 {
 public:
     Snake();
     virtual ~Snake();
 
-    QRectF boundingRect () const override;
+    /// \sa QGrpahicsObject::boundingRect
+    QRectF boundingRect() const override;
 
 protected:
-    void timerEvent(QTimerEvent *event);
-    void paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget);
+    /// \sa QObject::timerEvent
+    void timerEvent(QTimerEvent *event) override;
+
+    /// \sa QGraphicsObject::paint
+    void paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget) override;
 
 private:
-    static const int RECT_SIZE = 20;
-    static const int MAX_SIZE = 100;
-    static const int MIN_SIZE = 3;
-    static const int DELAY = 120;
+    /// \brief Possible states of a cell.
+    enum class CellT {
+        EMPTY = 0, FOOD, SNAKE_HEAD, SNAKE_TAIL
+    };
 
-    int x[MAX_SIZE];
-    int y[MAX_SIZE];
-    int direction;
-    int tail;
-    int timerId;
+    /// \brief Direction in which snake is moving
+    enum class Direction {
+        SOUTH, EAST, NORTH, WEST
+    };
 
-    void moveSnake();
+    /// \brief Framerate of the game
+    const unsigned fps = 60;
+    /// \brief Update rate of the game
+    const unsigned ups = 60;
+
+    Grid<CellT> grid;
+    Direction direction;
+
+    /// \brief Update the game state
+    void updateGame();
+    /// \brief Draw the game state
+    void drawGame();
+
+    /// \brief Set the current direction in which Snake is moving
+    /// \param d the Direction
+    /// \sa Snake::Direction
+    void moveSnake(Direction d);
 };
 
 #endif // SNAKE_H
